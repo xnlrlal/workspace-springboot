cbd3e957a8a7560e31fc8bc34df1efe6031a6757
xnlrlal <rlatjdgh227@gmail.com>
2023-02-13 21:25:08 +0900

    엔티티 클래스와 Querydsl 설정 및 사용방법

---
+++ demo/src/main/java/com/example/demo/repository/GuestbookRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
+}
