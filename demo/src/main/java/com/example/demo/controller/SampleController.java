6f7a9bc7f4bff4e14d8975594735a66a18c192c5
xnlrlal <rlatjdgh227@gmail.com>
2023-02-14 16:25:53 +0900

    파라미터 방식의 처리

--- demo/src/main/java/com/example/demo/controller/SampleController.java
+++ demo/src/main/java/com/example/demo/controller/SampleController.java
@@ -61,6 +61,6 @@ public class SampleController {
         log.info("ex3");
     }
 
-    @GetMapping("/exLayout1")
+    @GetMapping({"/exLayout1", "/exLayout2"})
     public void exLayout1() { log.info("exLayout.........."); }
 }
