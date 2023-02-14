27ee2eb29e2897ccfef0e0f3b34c1a8538164cb4
xnlrlal <rlatjdgh227@gmail.com>
2023-02-14 17:12:36 +0900

    레이아웃 템플릿 만들기
    layout1.html이 전체 레이아웃을 가지는 페이지
    exTemplate.html 필요한 부분만을 개발해서 템플릿으로 적용

--- demo/src/main/java/com/example/demo/controller/SampleController.java
+++ demo/src/main/java/com/example/demo/controller/SampleController.java
@@ -61,6 +61,6 @@ public class SampleController {
         log.info("ex3");
     }
 
-    @GetMapping({"/exLayout1", "/exLayout2"})
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
     public void exLayout1() { log.info("exLayout.........."); }
 }
