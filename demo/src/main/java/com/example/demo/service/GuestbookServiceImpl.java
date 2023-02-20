396dd2c5f555c54e2c8f767ccc44651c1840057e
xnlrlal <rlatjdgh227@gmail.com>
2023-02-20 20:13:58 +0900

    방명록 조회/작성/수정/삭제

---
+++ demo/src/main/java/com/example/demo/service/GuestbookServiceImpl.java
@@ -0,0 +1,72 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.GuestbookDTO;
+import com.example.demo.dto.PageRequestDTO;
+import com.example.demo.dto.PageResultDTO;
+import com.example.demo.entity.Guestbook;
+import com.example.demo.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor // 의존성 자동 주입
+public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository; // 반드시 final로 선언
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO--------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = repository.findById(gno);
+        return result.isPresent() ? entityToDto(result.get()) : null;
+    }
+
+    @Override
+    public void remove(Long gno) {
+        repository.deleteById(gno);
+    }
+
+    @Override
+    public void modify(GuestbookDTO dto) {
+        // 업데이트하는 항목은 '제목', '내용'
+        Optional<Guestbook> result = repository.findById(dto.getGno());
+        if(result.isPresent()) {
+            Guestbook entity = result.get();
+
+            entity.changeTitle(dto.getTitle());
+            entity.changeContent(dto.getContent());
+
+            repository.save(entity);
+        }
+    }
+}
---
+++ demo/src/test/java/com/example/demo/service/GuestbookServiceTests.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.GuestbookDTO;
+import com.example.demo.dto.PageRequestDTO;
+import com.example.demo.dto.PageResultDTO;
+import com.example.demo.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister() {
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("----------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+}
